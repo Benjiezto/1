05bc88cf27b7d98495e194f7d382a74d18e18341
Benjiezto <a96186a@gmail.com>
2024-04-08 23:47:37 +0800

    3-1 3-2
    計算機

---
+++ 3-1 3-2/3-1 3-2/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3_1_3_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtCM_KeyUp(object sender, KeyEventArgs e)
+        {
+            double douCM; //宣告一個double變數，變數名稱叫douCM
+
+            douCM = Convert.ToDouble(txtCM.Text); //從txtCM輸入文字框取得輸入的文字，並且轉換成double的資料型態
+
+            label3.Text = string.Format("{0:0.##########}", douCM / 100);
+            //將douCM的數值除以100，也就是從公分轉換成公尺
+            //透過string.Format格式化成小數點後共10位的數字，轉型成文字型態，在txtM顯示結果
+        }
+    }
+}
