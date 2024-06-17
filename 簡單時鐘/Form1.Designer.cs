0e4bb86d3f1785e397693f17628270e7c142f802
Benjiezto <a96186a@gmail.com>
2024-06-17 14:31:07 +0800

    6/17簡單時鐘
    簡單時鐘的音效+碼錶功能

--- 簡單時鐘/Form1.Designer.cs
+++ 簡單時鐘/Form1.Designer.cs
@@ -33,6 +33,18 @@ namespace SimpleClock
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.bindingNavigator1 = new System.Windows.Forms.BindingNavigator(this.components);
+            this.bindingNavigatorAddNewItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorCountItem = new System.Windows.Forms.ToolStripLabel();
+            this.bindingNavigatorDeleteItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMoveFirstItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMovePreviousItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorPositionItem = new System.Windows.Forms.ToolStripTextBox();
+            this.bindingNavigatorSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorMoveNextItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMoveLastItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator2 = new System.Windows.Forms.ToolStripSeparator();
             this.btnCancelAlert = new System.Windows.Forms.Button();
             this.btnSetAlert = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
@@ -42,11 +54,22 @@ namespace SimpleClock
             this.txtDate = new System.Windows.Forms.TextBox();
             this.txtTime = new System.Windows.Forms.TextBox();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.txtStopWatch = new System.Windows.Forms.TextBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnLog = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.listStopWatchLog = new System.Windows.Forms.ListBox();
             this.tabPage3 = new System.Windows.Forms.TabPage();
             this.timerClcok = new System.Windows.Forms.Timer(this.components);
             this.timerAlert = new System.Windows.Forms.Timer(this.components);
+            this.timerStopWatch = new System.Windows.Forms.Timer(this.components);
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingNavigator1)).BeginInit();
+            this.bindingNavigator1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -63,6 +86,7 @@ namespace SimpleClock
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.bindingNavigator1);
             this.tabPage1.Controls.Add(this.btnCancelAlert);
             this.tabPage1.Controls.Add(this.btnSetAlert);
             this.tabPage1.Controls.Add(this.label1);
@@ -78,6 +102,121 @@ namespace SimpleClock
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "時鐘";
             this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // bindingNavigator1
+            // 
+            this.bindingNavigator1.AddNewItem = this.bindingNavigatorAddNewItem;
+            this.bindingNavigator1.CountItem = this.bindingNavigatorCountItem;
+            this.bindingNavigator1.DeleteItem = this.bindingNavigatorDeleteItem;
+            this.bindingNavigator1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.bindingNavigator1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bindingNavigatorMoveFirstItem,
+            this.bindingNavigatorMovePreviousItem,
+            this.bindingNavigatorSeparator,
+            this.bindingNavigatorPositionItem,
+            this.bindingNavigatorCountItem,
+            this.bindingNavigatorSeparator1,
+            this.bindingNavigatorMoveNextItem,
+            this.bindingNavigatorMoveLastItem,
+            this.bindingNavigatorSeparator2,
+            this.bindingNavigatorAddNewItem,
+            this.bindingNavigatorDeleteItem});
+            this.bindingNavigator1.Location = new System.Drawing.Point(3, 3);
+            this.bindingNavigator1.MoveFirstItem = this.bindingNavigatorMoveFirstItem;
+            this.bindingNavigator1.MoveLastItem = this.bindingNavigatorMoveLastItem;
+            this.bindingNavigator1.MoveNextItem = this.bindingNavigatorMoveNextItem;
+            this.bindingNavigator1.MovePreviousItem = this.bindingNavigatorMovePreviousItem;
+            this.bindingNavigator1.Name = "bindingNavigator1";
+            this.bindingNavigator1.PositionItem = this.bindingNavigatorPositionItem;
+            this.bindingNavigator1.Size = new System.Drawing.Size(921, 27);
+            this.bindingNavigator1.TabIndex = 8;
+            this.bindingNavigator1.Text = "bindingNavigator1";
+            // 
+            // bindingNavigatorAddNewItem
+            // 
+            this.bindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorAddNewItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorAddNewItem.Image")));
+            this.bindingNavigatorAddNewItem.Name = "bindingNavigatorAddNewItem";
+            this.bindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorAddNewItem.Size = new System.Drawing.Size(29, 24);
+            this.bindingNavigatorAddNewItem.Text = "加入新的";
+            // 
+            // bindingNavigatorCountItem
+            // 
+            this.bindingNavigatorCountItem.Name = "bindingNavigatorCountItem";
+            this.bindingNavigatorCountItem.Size = new System.Drawing.Size(34, 24);
+            this.bindingNavigatorCountItem.Text = "/{0}";
+            this.bindingNavigatorCountItem.ToolTipText = "項目總數";
+            // 
+            // bindingNavigatorDeleteItem
+            // 
+            this.bindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorDeleteItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorDeleteItem.Image")));
+            this.bindingNavigatorDeleteItem.Name = "bindingNavigatorDeleteItem";
+            this.bindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorDeleteItem.Size = new System.Drawing.Size(29, 24);
+            this.bindingNavigatorDeleteItem.Text = "刪除";
+            // 
+            // bindingNavigatorMoveFirstItem
+            // 
+            this.bindingNavigatorMoveFirstItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveFirstItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveFirstItem.Image")));
+            this.bindingNavigatorMoveFirstItem.Name = "bindingNavigatorMoveFirstItem";
+            this.bindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveFirstItem.Size = new System.Drawing.Size(29, 24);
+            this.bindingNavigatorMoveFirstItem.Text = "移到最前面";
+            // 
+            // bindingNavigatorMovePreviousItem
+            // 
+            this.bindingNavigatorMovePreviousItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMovePreviousItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMovePreviousItem.Image")));
+            this.bindingNavigatorMovePreviousItem.Name = "bindingNavigatorMovePreviousItem";
+            this.bindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMovePreviousItem.Size = new System.Drawing.Size(29, 24);
+            this.bindingNavigatorMovePreviousItem.Text = "移到上一個";
+            // 
+            // bindingNavigatorSeparator
+            // 
+            this.bindingNavigatorSeparator.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator.Size = new System.Drawing.Size(6, 27);
+            // 
+            // bindingNavigatorPositionItem
+            // 
+            this.bindingNavigatorPositionItem.AccessibleName = "位置";
+            this.bindingNavigatorPositionItem.AutoSize = false;
+            this.bindingNavigatorPositionItem.Font = new System.Drawing.Font("Microsoft JhengHei UI", 9F);
+            this.bindingNavigatorPositionItem.Name = "bindingNavigatorPositionItem";
+            this.bindingNavigatorPositionItem.Size = new System.Drawing.Size(50, 27);
+            this.bindingNavigatorPositionItem.Text = "0";
+            this.bindingNavigatorPositionItem.ToolTipText = "目前的位置";
+            // 
+            // bindingNavigatorSeparator1
+            // 
+            this.bindingNavigatorSeparator1.Name = "bindingNavigatorSeparator1";
+            this.bindingNavigatorSeparator1.Size = new System.Drawing.Size(6, 27);
+            // 
+            // bindingNavigatorMoveNextItem
+            // 
+            this.bindingNavigatorMoveNextItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveNextItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveNextItem.Image")));
+            this.bindingNavigatorMoveNextItem.Name = "bindingNavigatorMoveNextItem";
+            this.bindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveNextItem.Size = new System.Drawing.Size(29, 24);
+            this.bindingNavigatorMoveNextItem.Text = "移到下一個";
+            // 
+            // bindingNavigatorMoveLastItem
+            // 
+            this.bindingNavigatorMoveLastItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveLastItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveLastItem.Image")));
+            this.bindingNavigatorMoveLastItem.Name = "bindingNavigatorMoveLastItem";
+            this.bindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveLastItem.Size = new System.Drawing.Size(29, 24);
+            this.bindingNavigatorMoveLastItem.Text = "移到最後面";
+            // 
+            // bindingNavigatorSeparator2
+            // 
+            this.bindingNavigatorSeparator2.Name = "bindingNavigatorSeparator2";
+            this.bindingNavigatorSeparator2.Size = new System.Drawing.Size(6, 27);
             // 
             // btnCancelAlert
             // 
@@ -159,6 +298,13 @@ namespace SimpleClock
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.txtStopWatch);
+            this.tabPage2.Controls.Add(this.btnStop);
+            this.tabPage2.Controls.Add(this.btnLog);
+            this.tabPage2.Controls.Add(this.btnReset);
+            this.tabPage2.Controls.Add(this.btnPause);
+            this.tabPage2.Controls.Add(this.btnStart);
+            this.tabPage2.Controls.Add(this.listStopWatchLog);
             this.tabPage2.Location = new System.Drawing.Point(4, 38);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
@@ -166,6 +312,79 @@ namespace SimpleClock
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "碼表";
             this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // txtStopWatch
+            // 
+            this.txtStopWatch.Enabled = false;
+            this.txtStopWatch.Font = new System.Drawing.Font("微軟正黑體", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtStopWatch.Location = new System.Drawing.Point(6, 5);
+            this.txtStopWatch.Name = "txtStopWatch";
+            this.txtStopWatch.Size = new System.Drawing.Size(915, 167);
+            this.txtStopWatch.TabIndex = 12;
+            this.txtStopWatch.Text = "00:00:00:000";
+            this.txtStopWatch.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.Brown;
+            this.btnStop.Location = new System.Drawing.Point(408, 296);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(340, 50);
+            this.btnStop.TabIndex = 11;
+            this.btnStop.Text = "停止並歸零";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnLog
+            // 
+            this.btnLog.Location = new System.Drawing.Point(590, 243);
+            this.btnLog.Name = "btnLog";
+            this.btnLog.Size = new System.Drawing.Size(158, 50);
+            this.btnLog.TabIndex = 10;
+            this.btnLog.Text = "紀錄";
+            this.btnLog.UseVisualStyleBackColor = true;
+            this.btnLog.Click += new System.EventHandler(this.btnLog_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(408, 240);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(158, 50);
+            this.btnReset.TabIndex = 9;
+            this.btnReset.Text = "歸零";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.BackColor = System.Drawing.Color.Yellow;
+            this.btnPause.ForeColor = System.Drawing.Color.Black;
+            this.btnPause.Location = new System.Drawing.Point(590, 184);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(158, 53);
+            this.btnPause.TabIndex = 8;
+            this.btnPause.Text = "暫停";
+            this.btnPause.UseVisualStyleBackColor = false;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(408, 187);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(158, 50);
+            this.btnStart.TabIndex = 7;
+            this.btnStart.Text = "開始";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // listStopWatchLog
+            // 
+            this.listStopWatchLog.FormattingEnabled = true;
+            this.listStopWatchLog.ItemHeight = 29;
+            this.listStopWatchLog.Location = new System.Drawing.Point(15, 178);
+            this.listStopWatchLog.Name = "listStopWatchLog";
+            this.listStopWatchLog.Size = new System.Drawing.Size(271, 294);
+            this.listStopWatchLog.TabIndex = 0;
             // 
             // tabPage3
             // 
@@ -182,7 +401,11 @@ namespace SimpleClock
             // 
             // timerAlert
             // 
-            this.timerAlert.Tick += new System.EventHandler(this.timerAlert_Tick);
+            this.timerAlert.Tick += new System.EventHandler(this.timerAlert_tick);
+            // 
+            // timerStopWatch
+            // 
+            this.timerStopWatch.Tick += new System.EventHandler(this.timerStopWatch_tick);
             // 
             // Form1
             // 
@@ -196,6 +419,11 @@ namespace SimpleClock
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingNavigator1)).EndInit();
+            this.bindingNavigator1.ResumeLayout(false);
+            this.bindingNavigator1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -216,6 +444,26 @@ namespace SimpleClock
         private System.Windows.Forms.TextBox txtDate;
         private System.Windows.Forms.Timer timerClcok;
         private System.Windows.Forms.Timer timerAlert;
+        private System.Windows.Forms.BindingNavigator bindingNavigator1;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorAddNewItem;
+        private System.Windows.Forms.ToolStripLabel bindingNavigatorCountItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorDeleteItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveFirstItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMovePreviousItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator;
+        private System.Windows.Forms.ToolStripTextBox bindingNavigatorPositionItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator1;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveNextItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveLastItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator2;
+        private System.Windows.Forms.TextBox txtStopWatch;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnLog;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox listStopWatchLog;
+        private System.Windows.Forms.Timer timerStopWatch;
     }
 }
 
--- 簡單時鐘/Form1.cs
+++ 簡單時鐘/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NAudio.Wave;           // 音效檔播放器函式庫
+using System.IO;             // 檔案讀取的IO函式庫
+using System.Diagnostics;    // 引用「系統診斷」的函式庫
 
 namespace SimpleClock
 {
@@ -22,7 +25,15 @@ namespace SimpleClock
         }
 
         List<string> hours = new List<string>();            // 小時清單
-        List<string> minutes = new List<string>();          // 分鐘清單   
+        List<string> minutes = new List<string>();          // 分鐘清單 
+
+        string strSelectTime = "";                          // 用來記錄鬧鐘設定時間
+
+        private WaveOutEvent waveOut;                       // 音效檔播放器
+        private AudioFileReader audioFileReader;            // 音效檔讀取器
+
+        List<string> StopWatchLog = new List<string>();         // 碼表紀錄清單 
+        Stopwatch sw = new Stopwatch();                         // 宣告一個碼表物件
 
         // 下拉選單初始化
         private void comboboxInitialzation()
@@ -38,7 +49,7 @@ namespace SimpleClock
             cmbMin.SelectedIndex = 0;
         }
 
-        // 時鐘timer1_Tick事件：每0.1秒執行一次
+        // 時鐘timer1_Tick事件：每一秒執行一次
         private void timerClcok_Tick(object sender, EventArgs e)
         {
             txtTime.Text = DateTime.Now.ToString("HH:mm:ss");    // 顯示時間
@@ -46,9 +57,134 @@ namespace SimpleClock
             txtWeekDay.Text = DateTime.Now.ToString("dddd");     // 顯示星期幾
         }
 
-        private void timerAlert_Tick(object sender, EventArgs e)
+        // 鬧鐘計時器timerAlert_tick事件：每一秒執行一次
+        private void timerAlert_tick(object sender, EventArgs e)
         {
+            // 判斷現在時間是不是已經是鬧鐘設定時間？如果時間到了，就要播放鬧鐘聲音
+            if (strSelectTime == DateTime.Now.ToString("HH:mm"))
+            {
+                try
+                {
+                    stopWaveOut();
 
+                    // 指定聲音檔的相對路徑，可以使用MP3
+                    string audioFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources", "alert.wav");
+
+                    // 使用 AudioFileReader 來讀取聲音檔
+                    audioFileReader = new AudioFileReader(audioFilePath);
+
+                    // 初始化 WaveOutEvent
+                    waveOut = new WaveOutEvent();
+                    waveOut.Init(audioFileReader);
+
+                    // 播放聲音檔
+                    waveOut.Play();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("無法播放聲音檔，錯誤資訊: " + ex.Message);
+                }
+                finally
+                {
+                    timerAlert.Stop(); // 停止鬧鐘計時器
+                }
+            }
+        }
+
+        private void stopWaveOut()
+        {
+            // 停止之前的播放
+            if (waveOut != null)
+            {
+                waveOut.Stop();
+                waveOut.Dispose();
+                waveOut = null;
+            }
+        }
+
+        // 啟動鬧鐘
+        private void btnSetAlert_Click(object sender, EventArgs e)
+        {
+            timerAlert.Start(); // 啟動鬧鐘計時器
+            btnSetAlert.Enabled = false;
+            btnCancelAlert.Enabled = true;
+            strSelectTime = cmbHour.SelectedItem.ToString() + ":" + cmbMin.SelectedItem.ToString(); // 擷取小時和分鐘的下拉選單文字，用來設定鬧鐘時間
+        }
+
+        // 停止鬧鐘
+        private void btnCancelAlert_Click(object sender, EventArgs e)
+        {
+            stopWaveOut();     // 停止之前的播放
+            timerAlert.Stop(); // 停止鬧鐘計時器
+            btnSetAlert.Enabled = true;
+            btnCancelAlert.Enabled = false;
+        }
+
+        // timerStopWatch_tick：每毫秒執行一次，所以更新的速度會比較快
+        private void timerStopWatch_tick(object sender, EventArgs e)
+        {
+            txtStopWatch.Text = sw.Elapsed.ToString("hh':'mm':'ss':'fff");    // 顯示碼表時間
+        }
+
+        // 啟動碼表
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            sw.Start();             // 啟動碼表
+            timerStopWatch.Start(); // 開始讓碼表文字顯示
+        }
+
+        // 停止並歸零碼表
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            sw.Reset();                           // 停止並歸零碼表
+            timerStopWatch.Stop();                // 停止讓碼表文字顯示     
+            txtStopWatch.Text = "00:00:00:000";   // 讓碼表文字「歸零」
+            listStopWatchLog.Items.Clear();       // 清空 ListBox 中的元素
+            StopWatchLog.Clear();                 // 清除暫存碼表紀錄清單
+        }
+
+        // 歸零按鍵會判斷你是否先按下暫停？來決定是否記錄碼表時間
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            // 如果碼表還在跑，就紀錄目前的時間，最後歸零再啟動碼錶
+            if (sw.IsRunning)
+            {
+                logRecord();
+                sw.Restart(); // 歸零碼表，碼表仍繼續進行  
+            }
+            else
+            {
+                sw.Reset(); // 如果碼表沒在跑，停止並歸零碼表
+                txtStopWatch.Text = "00:00:00:000";   // 讓碼表文字「歸零」
+            }
+        }
+
+        // 停止碼表
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            sw.Stop();                  // 停止碼表，但不歸零
+            timerStopWatch.Stop();      // 停止讓碼表文字顯示  
+        }
+
+        // 碼表時間紀錄
+        private void btnLog_Click(object sender, EventArgs e)
+        {
+            logRecord();
+        }
+
+        // 碼表時間紀錄
+        private void logRecord()
+        {
+            listStopWatchLog.Items.Clear(); // 清空 ListBox 中的元素
+            StopWatchLog.Add(txtStopWatch.Text); // 將碼表時間增加到暫存碼表紀錄清單裡
+
+            // 依照碼表紀錄清單「依照最新時間順序」顯示
+            int i = StopWatchLog.Count;
+            while (i > 0)
+            {
+                listStopWatchLog.Items.Add(String.Format("第 {0} 筆紀錄：{1}", i.ToString(), StopWatchLog[i - 1] + "\n"));
+                i--;
+            }
         }
     }
 }
